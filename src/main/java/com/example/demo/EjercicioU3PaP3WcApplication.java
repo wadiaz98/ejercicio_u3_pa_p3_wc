e3ad85aa2c7423f001be60e32a4e001d33b23508
Willan Diaz <wadiaz@uce.edu.ec>
2023-02-17 18:12:13 -0500

    Subida de proyecto del ejercicio

---
+++ src/main/java/com/example/demo/EjercicioU3PaP3WcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioU3PaP3WcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioU3PaP3WcApplication.class, args);
+	}
+
+}
